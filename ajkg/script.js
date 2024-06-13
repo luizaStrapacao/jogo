c68deb588fd4ab1c9f7fc481db21b31359014a6a
luiza <104519399+luizaStrapacao@users.noreply.github.com>
2024-06-13 11:30:13 -0300

    script.js

--- ajkg/script.js
+++ ajkg/script.js
@@ -1,38 +1,38 @@
 const perguntas = [
     {
-    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+    enunciado: "Você concorda com a legalização da maconha para uso recreativo? ",
     alternativas: [
-    "Eles vão dominar o mundo!",
-    "Isso é maravilhoso, vamos ter mais facilidade!"
+    "Sim, acredito que a legalização poderia reduzir o crime relacionado às drogas.",
+    "Não, acredito que poderia aumentar o consumo de drogas e causar problemas de saúde pública."
     ]
     },
     {
-    enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+    enunciado: "Você concorda com a proibição de armas de fogo?",
     alternativas: [
-    "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações.",
-    "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema (prefere métodos arcaicos)."
+    "Sim, acho que isso pode diminuir a violência armada e proteger a segurança pública.",
+    "Não, acredito que as pessoas têm o direito de se defender e que a proibição violaria direitos constitucionais."
     ]
     },
     {
-    enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+    enunciado: "Você concorda com a implementação de cotas raciais em universidades?",
     alternativas: [
-    "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-    "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores. Sou totalmente contra!"
+    "Sim, acredito que é necessário para promover a igualdade de oportunidades para grupos historicamente marginalizados.",
+    "Não, acho que as cotas podem criar injustiças e discriminação reversa."
     ]
     },
     {
-    enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+    enunciado: "Você concorda com a redução da idade mínima para votar para 16 anos?",
     alternativas: [
-    "Criar uma imagem utilizando uma plataforma de design como o Paint pois IA acaba com trabalho de artistas.",
-    "Criar uma imagem utilizando um gerador de imagem de IA e não liga para as consequencias."
+    "Sim, acredito que os jovens devem ter voz nas decisões políticas que afetam seu futuro.",
+    "Não, acho que os jovens podem não ter maturidade suficiente para tomar decisões políticas informadas."
     ]
     
     },
     {
-    enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
+    enunciado: "Você concorda com a pena de morte para crimes graves?",
     alternativas: [
-    "Escrever comandos para o chat e nem le para não ter trabalho",
-    "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial, além de não ser uma fonte legitimada."
+    "Sim, acredito que a pena de morte é uma forma de justiça e serve como um deterrente eficaz.",
+    "Não, acredito que a pena de morte é desumana e falível, podendo levar à execução de inocentes."
     ]
     }
     ];
